e508318d207e344f294f0d570176940ee5643901
HTML-Experts <mq30994@gmail.com>
2023-11-17 15:40:18 -1200

    styling: pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -13,10 +13,10 @@ const Pricing = () => {
   return (
     <div
       id="pricing"
-      className="lg:container m-auto lg:px-0 px-3 md:pt-24 pt-16 border-b md:pb-28 pb-14 "
+      className="lg:container m-auto lg:px-0 px-3 md:pt-24 pt-20 border-b md:pb-28 pb-14 "
     >
-      <div className="flex md:felx-row flex-col md:gap-2 gap-4 justify-between ">
-        <div className="features flex flex-col gap-4 md:text-left text-center">
+      <div className=" relative flex md:flex-row flex-col md:gap-2 gap-4 justify-between ">
+        <div className="features md:sticky  top-36 flex flex-col md:gap-4 ga-2 h-[100%] text-center md:w-[44%] w-full">
           <h3 className="text-[--headingColor] font-[500] uppercase text-lg">
             pricing
           </h3>
@@ -26,16 +26,19 @@ const Pricing = () => {
         </div>
 
         {/*  */}
-        <div className="portfolioCard md:w-[51.3%] w-[94.3%] m-auto">
-          <div className="flex flex-wrap resumTabs">
+        <div className="portfolioCard md:w-[44.3%] w-[100%] m-auto">
+          <div className="flex md:flex-nowrap justify-between items-center resumTabs">
             {pricing.map((tab) => (
               <div
                 key={tab.id}
-                className={`cursor-pointer p-3 resumBtn text-[24px] font-[600] py-6 px-[60px] 
+                className={`w-full flex text-center cursor-pointer md:p-1 p-0 resumBtn 
                  ${tab.id === activetabs ? "activetab" : ""} `}
                 onClick={() => handleTabclick(tab.id)}
               >
-                {tab.tabBtn}
+                <h1 className="md:text-[24px] text-sm font-[600] md:py-6 py-5 w-full px-[17px]">
+                  {" "}
+                  {tab.tabBtn}
+                </h1>
               </div>
             ))}
           </div>
@@ -44,38 +47,87 @@ const Pricing = () => {
             {pricing.map((tabs) => (
               <div key={tabs.id}>
                 {activetabs === tabs.id && (
-                  <div className="p-7">
-                    <div className="flex justify-between items-center">
-                      <div className="flex flex-col">
-                        <h1 className="color-[--headingColor] flex flex-col md:gap-3 gap-0 lg:text-xl leading-tight sm:text-sm text-lg uppercase font-[700] ">
+                  <div className="flex flex-col md:gap-5 gap-5 px-7 py-7">
+                    <div className="flex justify-between items-center gap-2">
+                      <div className="flex flex-col gap-1">
+                        <h1 className="color-[--headingColor] flex flex-col md:gap-3 gap-0 lg:text-xl leading-tight sm:text-sm text-sm uppercase md:font-[700] font-[600]">
                           {tabs.title}
                         </h1>
-                        <h3 className="text-[--headingColor] font-[500] uppercase text-lg">
+                        <h3 className="text-[--headingColor] font-[500] uppercase md:text-lg text-sm">
                           {tabs.subTitle}
                         </h3>
                       </div>
                       <div>
-                        <h1 className="color-[--headingColor] flex flex-col md:gap-3 gap-0 lg:text-xl leading-tight sm:text-sm text-lg uppercase font-[700]">
+                        <h1 className="contactshadow px-3 py-2 color-[--headingColor] flex flex-col md:gap-3 gap-0 lg:text-xl leading-tight sm:text-sm text-sm uppercase font-[700]">
                           ${tabs.price}.00
                         </h1>
                       </div>
                     </div>
                     <div>
-                      <p className="flex md:w-[60%]">{tabs.detail}</p>
+                      <p className="flex md:w-[60%] md:text-sm text-xs">
+                        {tabs.detail}
+                      </p>
                       {/* <React.Fragment>
                           <p>{line}</p>
                         </React.Fragment> */}
                     </div>
-                    <div className="flex flex-col">
-                      <ul className="flex justify-between items-center">
-                        <li>{tabs.li1}</li>
-                        <li>{tabs.li2}</li>
+                    <div className="pricingCard flex flex-col gap-3 px-5">
+                      <ul className="flex sm:flex-row justify-between items-center">
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li1}
+                        </li>
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li2}
+                        </li>
                       </ul>
-                      <div className="flex justify-between items-center"></div>
-                      <div className="flex justify-between items-center"></div>
-                      <div className="flex justify-between items-center"></div>
-                      <div className="flex justify-between items-center"></div>
-                      <div className="flex justify-between items-center"></div>
+                      <ul className="flex sm:flex-row justify-between items-center">
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li3}
+                        </li>
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li4}
+                        </li>
+                      </ul>
+                      <ul className="flex sm:flex-row justify-between items-center">
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li5}
+                        </li>
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li6}
+                        </li>
+                      </ul>
+                      <ul className="flex sm:flex-row justify-between items-center">
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li7}
+                        </li>
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li8}
+                        </li>
+                      </ul>
+                      <ul className="flex sm:flex-row justify-between items-center">
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li9}
+                        </li>
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li10}
+                        </li>
+                      </ul>
+                      <ul className="flex sm:flex-row justify-between items-center">
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li11}
+                        </li>
+                        <li className="text-[--headingColor] md:text-lg text-sm">
+                          {tabs.li12}
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="md:pt-7">
+                      <button
+                        type="submit"
+                        className="contactBtn w-full cursor-pointer"
+                      >
+                        order now
+                      </button>
                     </div>
                   </div>
                 )}
